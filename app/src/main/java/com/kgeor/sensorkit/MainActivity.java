335088b8232347e87c7e92b8a7441af7fdcf915b
Keegan George <kgeorge13@gmail.com>
2018-06-06 11:06:26 -0700

    Created Android Project

---
+++ app/src/main/java/com/kgeor/sensorkit/MainActivity.java
@@ -0,0 +1,17 @@
+package com.kgeor.sensorkit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author Keegan George
+ * @version 1.0
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
