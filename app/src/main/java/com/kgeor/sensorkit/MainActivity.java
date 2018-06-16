ccab4127eaf39faf6cddf566064af0d0c802aea2
Keegan George <kgeorge13@gmail.com>
2018-06-16 00:34:19 -0700

    Update main design and implementation

--- app/src/main/java/com/kgeor/sensorkit/MainActivity.java
+++ app/src/main/java/com/kgeor/sensorkit/MainActivity.java
@@ -1,18 +1,21 @@
 package com.kgeor.sensorkit;
 
 
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 
 
 /**
+ * Main Activity Class
+ *
  * @author Keegan George
  * @version 1.0
  */
@@ -21,24 +24,104 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private BottomNavigationView mMainNav;
     private FrameLayout mMainFrame;
+    public static final int REQUEST_CODE = 200;
+
+
+    // REFERENCES TO FRAGMENTS //
     private HomeFragment homeFragment;
     private EnviroFragment environmentFragment;
     private LightFragment lightFragment;
     private MoveFragment movementFragment;
+    private VibrationFragment vibrationFragment;
+
+    // PERMISSIONS //
+    private boolean permissionRecordAudio = false;
+    private boolean permissionWriteAudio = false;
+    private String[] permissions = {
+            "android.permission.RECORD_AUDIO", "android.permission.WRITE_EXTERNAL_STORAGE"
+    };
+
+
+    /**
+     * Responsible for requesting permissions to user
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // what to do when permission is granted
+        switch (REQUEST_CODE) {
+            case 200:
+                permissionRecordAudio = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                permissionWriteAudio = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+
+        // what to do when permission is denied
+        if (!permissionRecordAudio) {
+            MainActivity.super.finish();
+        }
+
+        if (!permissionWriteAudio) {
+            MainActivity.super.finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "onCreate: started."); // TODO Remove before deployment
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(permissions, REQUEST_CODE);
+        }
+
+        // link XML to Java
         mMainFrame = findViewById(R.id.main_frame);
         mMainNav = findViewById(R.id.main_nav);
+        initFragments();
+    } // onCreate() end
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    /**
+     * Responsible for settings the title of the action bar
+     *
+     * @param title title to be set
+     */
+    public void setActionBarTitle(String title) {
+        getSupportActionBar().setTitle(title);
+    }
+
+    /**
+     * Responsible for settings fragment
+     *
+     * @param fragment desired fragment
+     */
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction;
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
+    }
+
+    /**
+     * Create new fragment objects
+     * Control action of bottom bar
+     */
+    protected void initFragments() {
         homeFragment = new HomeFragment();
         environmentFragment = new EnviroFragment();
         lightFragment = new LightFragment();
         movementFragment = new MoveFragment();
+        vibrationFragment = new VibrationFragment();
 
         setFragment(homeFragment);
 
@@ -53,21 +136,24 @@ public class MainActivity extends AppCompatActivity {
                         setFragment(homeFragment);
                         return true;
 
-                    case R.id.nav_environment:
-                        mMainNav.setItemBackgroundResource(R.color.colorPrimary);
-                        setFragment(environmentFragment);
-                        return true;
-
-
                     case R.id.nav_light:
                         mMainNav.setItemBackgroundResource(R.color.colorPrimary);
                         setFragment(lightFragment);
                         return true;
 
+                    case R.id.nav_vibration:
+                        mMainNav.setItemBackgroundResource(R.color.colorPrimary);
+                        setFragment(vibrationFragment);
+                        return true;
 
                     case R.id.nav_movement:
                         mMainNav.setItemBackgroundResource(R.color.colorPrimary);
                         setFragment(movementFragment);
+                        return true;
+
+                    case R.id.nav_environment:
+                        mMainNav.setItemBackgroundResource(R.color.colorPrimary);
+                        setFragment(environmentFragment);
                         return true;
 
                     default:
@@ -77,25 +163,5 @@ public class MainActivity extends AppCompatActivity {
 
 
         });
-
-
-    } // onCreate() end
-
-
-    private void setFragment(Fragment fragment) {
-        FragmentTransaction fragmentTransaction;
-        fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.main_frame, fragment);
-        fragmentTransaction.commit();
     }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-}
+} // class end
--- app/src/main/java/com/kgeor/sensorkit/SensorDetails.java
+++ app/src/main/java/com/kgeor/sensorkit/SensorDetails.java
@@ -1,15 +1,95 @@
 package com.kgeor.sensorkit;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
-public class SensorDetails extends AppCompatActivity {
 
+import java.util.List;
+
+/**
+ * Responsible for expanded details of a specific sensor
+ */
+public class SensorDetails extends AppCompatActivity implements SensorEventListener {
+    // FIELDS //
+    TextView sensorInfo, sensorValues; // xml references
+    SensorManager mSensorManager; // reference to sensor manager
+    Sensor sensor = null; // sensor is initially null
+    List<Sensor> sensorList; // list of sensors on device
+
+    // METHODS //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor_details);
 
-        String itemId = getIntent().getExtras().getString(DataItemAdapter.ITEM_ID_KEY);
+        // acquire sensors
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensorList = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        // link XML to Java
+        sensorInfo = findViewById(R.id.sensor_details);
+        sensorValues = findViewById(R.id.sensor_values);
+
+        sensor = mSensorManager.getDefaultSensor(sensorList.get(getIntent().getExtras().getInt("sensorKey")).getType());
+        // update action bar title to selected sensor's name
+        getSupportActionBar().setTitle(sensor.getName());
+
+        // string for sensor details
+        String sensorDetails =
+                "<b>Name:</b> \"" + sensor.getName() + "\"<br>" +
+                        "<b>Type:</b> \"" + sensor.getType() + "\"<br>" +
+                        "<b>Version:</b> \"" + sensor.getVersion() + "\"<br>" +
+                        "<b>Vendor:</b> \"" + sensor.getVendor() + "\"<br>" +
+                        "<b>Power:</b> \"" + sensor.getPower() + "\"<br>" +
+                        "<b>Resolution:</b> \"" + sensor.getResolution() + "\"<br>" +
+                        "<b>Minimum Delay:</b> \"" + sensor.getMinDelay() + "\"<br>" +
+                        "<b>Maximum Range:</b> \"" + sensor.getMaximumRange() + "\"<br>";
+        // set TextView based on receiving String from an HTML format
+        sensorInfo.setText(Html.fromHtml(sensorDetails));
     }
-}
+
+    @Override
+    protected void onPause() {
+        mSensorManager.unregisterListener(this);
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+
+    /**
+     * Responsible for events when sensor values are updated
+     *
+     * @param event current event
+     */
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // array of sensor values
+        float[] values = event.values;
+
+        String curVal = ""; // initial value
+
+        // loop through all values and display & update them in the text view
+        for (int i = 0; i < values.length; i++) {
+            curVal += Float.toString(values[i]) + "\n";
+        }
+        sensorValues.setText(curVal);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // TODO Auto-generated stub
+    }
+    
+} // class end
